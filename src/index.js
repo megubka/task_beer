96205616fab6bc921e3b6925f517becb45485641
Alimzhan Aigarayev <alimzhan.aigarayev@gmail.com>
2018-07-29 20:37:16 +0200

    infiniteScroll shortName spiner etc added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import reducers from 'reducers'
 
 import Root from 'containers/root/index'
@@ -14,6 +16,10 @@ const store = createStore(reducers, composeWithDevTools(
 ))
 
 ReactDOM.render(
-    <Root store={store} />,
+    <Provider store={store}>
+        <Router>
+            <Route component={Root} />
+        </Router>
+    </Provider>,
     document.getElementById('root')
 )
